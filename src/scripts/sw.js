ee0fbdf98d64fcdd1c7f89b584b71497dd8fb091
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2025-01-05 21:19:27 +0700

    rebuild project

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,31 @@
-// import 'regenerator-runtime';
-// import CacheHelper from './utils/cache-helper';
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 // const { assets } = global.serviceWorkerOption;
 
+const assetsToCache = [
+  './',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './index.html',
+  './bitespot_logo.png',
+  './app.bundle.js',
+  './manifest',
+  './sw.bundle.js',
+];
+
 self.addEventListener("install", (event) => {
-  console.log('Installing Service Worker ...');
-  // event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+  // console.log('Installing Service Worker ...');
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 self.addEventListener("activate", (event) => {
-  console.log('Activating Service Worker ...');
-  // event.waitUntil(CacheHelper.deleteOldCache());
+  // console.log('Activating Service Worker ...');
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
+  // console.log(event.request);
  
-  event.respondWith(fetch(event.request));
-  // event.respondWith(CacheHelper.revalidateCache(event.request));
+  // event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
